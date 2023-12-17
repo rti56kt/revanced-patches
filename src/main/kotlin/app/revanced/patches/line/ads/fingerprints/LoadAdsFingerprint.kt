99a4d0c09d1f11b881f4797113071658e33a95e8
rti56kt <rti56kt@gmail.com>
2023-12-17 16:22:46 +0800

    fix: change defining class of fingerprint

--- src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
@@ -7,11 +7,11 @@ import com.android.tools.smali.dexlib2.AccessFlags
 internal object LoadAdsFingerprint : MethodFingerprint(
     returnType = "V",
     strings = listOf(
-        "context",
-        "getApplicationContext(...)"
+        "CREATE TABLE IF NOT EXISTS `ads` (`rid_uaid` TEXT NOT NULL, `inventory_key` TEXT NOT NULL, `ad_total` INTEGER NOT NULL, `ad_order` INTEGER NOT NULL, `ad` TEXT NOT NULL, `state` TEXT NOT NULL, `expiration_time` INTEGER NOT NULL, PRIMARY KEY(`rid_uaid`))",
+        "INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, \'ea082e641b37724296378b9d25e31c36\')"
     ),
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     customFingerprint = { methodDef, _ ->
-        methodDef.definingClass == "Landroid/content/Context;"
+        methodDef.definingClass == "Lcom/linecorp/line/ladsdk/impl/internal/db/LadDatabase_Impl;"
     }
 )
