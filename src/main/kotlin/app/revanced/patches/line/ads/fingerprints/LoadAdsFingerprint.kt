83ef3ae5788194fb6a09e1e119be1f96edf3e8e6
rti56kt <rti56kt@gmail.com>
2023-12-17 16:56:23 +0800

    fix: fix fingerprint

--- src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
@@ -8,6 +8,7 @@ internal object LoadAdsFingerprint : MethodFingerprint(
     returnType = "V",
     strings = listOf(
         "CREATE TABLE IF NOT EXISTS `ads` (`rid_uaid` TEXT NOT NULL, `inventory_key` TEXT NOT NULL, `ad_total` INTEGER NOT NULL, `ad_order` INTEGER NOT NULL, `ad` TEXT NOT NULL, `state` TEXT NOT NULL, `expiration_time` INTEGER NOT NULL, PRIMARY KEY(`rid_uaid`))",
+        "CREATE TABLE IF NOT EXISTS `sent_event` (`rid_uaid` TEXT NOT NULL, `event_name` TEXT NOT NULL, `timestamp` INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)",
         "CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)"
     ),
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
