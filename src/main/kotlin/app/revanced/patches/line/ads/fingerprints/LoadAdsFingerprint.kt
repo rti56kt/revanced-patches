c85654f5b4cebd697dce5022b4c22aeadcc0ef29
rti56kt <rti56kt@gmail.com>
2023-12-17 16:50:10 +0800

    chore: change fingerprint

--- src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/line/ads/fingerprints/LoadAdsFingerprint.kt
@@ -8,7 +8,7 @@ internal object LoadAdsFingerprint : MethodFingerprint(
     returnType = "V",
     strings = listOf(
         "CREATE TABLE IF NOT EXISTS `ads` (`rid_uaid` TEXT NOT NULL, `inventory_key` TEXT NOT NULL, `ad_total` INTEGER NOT NULL, `ad_order` INTEGER NOT NULL, `ad` TEXT NOT NULL, `state` TEXT NOT NULL, `expiration_time` INTEGER NOT NULL, PRIMARY KEY(`rid_uaid`))",
-        "INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, \'ea082e641b37724296378b9d25e31c36\')"
+        "CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)"
     ),
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     customFingerprint = { methodDef, _ ->
